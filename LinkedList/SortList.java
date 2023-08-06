d5f4191bdeabfa266012716716c130354d73cb2b
rafipopz98 <itsrealrafi@gmail.com>
2023-08-06 22:30:19 +0530

    237 delete node

---
+++ LinkedList/SortList.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class SortList{
+    public static void main(String args[]){
+        LinkedList<Integer> a=new Linkedlist<Integer>(10);
+        a.add(2);
+        a.add(1);
+        a.add(4);
+        quicksort(a);
+    }static void quicksort(LinkedList<Integer>a){
+        if()
+    }
+}
